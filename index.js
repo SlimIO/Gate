e95774ddadb101a4ce13bb0ef6676371e76d8bfa
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-03-26 16:10:33 +0100

    feat: implement list addons & global core link

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+// Require Third-party Dependencies
 const Addon = require("@slimio/addon");
 
 const agent = new Addon("agent");
 
-// eslint-disable-next-line
-async function globalInfo(header) {
+// Retrieve global core
+const core = global.slimio_core;
+
+async function globalInfo() {
     return {
+        root: core.root,
         coreVersion: process.env.coreVersion
     };
 }
 
-agent.on("start", () => {
+async function listAddons() {
+    return [...core.addons.keys()];
+}
 
+agent.on("start", () => {
+    console.log("agent started!");
+    console.log(core);
 });
 
 agent.registerCallback("global_info", globalInfo);
+agent.registerCallback("list_addons", listAddons);
 
 module.exports = agent;
