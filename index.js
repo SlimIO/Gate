65bc28420b466b7f28cf89a8f8bdd99050381db4
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-07-05 17:18:05 +0200

    chore: add agent.json configuration callbacks

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Require Node.js Dependencies
+const { readFile } = require("fs").promises;
+const { join } = require("path");
+
 // Require Third-party Dependencies
 const Addon = require("@slimio/addon");
 
@@ -22,6 +26,21 @@ async function getRoutingTable() {
     return [...core.routingTable.keys()];
 }
 
+async function getConfig(header, path) {
+    if (typeof path === "string") {
+        return core.config.get(path);
+    }
+
+    return core.config.payload;
+}
+
+async function setConfig(header, path, value) {
+    const { config } = core;
+    config.set(path, value);
+    config.lazyWriteOnDisk();
+}
+
+
 gate.on("start", async() => {
     await gate.ready();
 });
@@ -29,5 +48,9 @@ gate.on("start", async() => {
 gate.registerCallback("global_info", globalInfo);
 gate.registerCallback("list_addons", listAddons);
 gate.registerCallback("get_routing_table", getRoutingTable);
+
+// Agent.json configuration
+gate.registerCallback("get_config", getConfig);
+gate.registerCallback("set_config", setConfig);
 
 module.exports = gate;
